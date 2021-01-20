cd150be8d5e58ce3d693ac8b372eb04905c9a60b
JLA <63759042+Blu3DrAEm@users.noreply.github.com>
2021-01-20 13:36:07 -0700

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -2,8 +2,8 @@
 # run "sudo apt-get install expect" to make sure expect is installed
 # please note the '\r' is required at the end of send message for expect script to work properly
 spawn ./openvpn.sh
-expect "sudo"
-send "raspberry\r"
+#expect "sudo"    # uncomment line 5 and 6 if you're using an account other than "Pi"
+#send "raspberry\r"   # replace 'raspberry' with correct password if you change your admin password
 expect "Username"
 send "vpnbook\r"
 expect "Password"
