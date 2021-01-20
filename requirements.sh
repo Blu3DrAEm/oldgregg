a401d3957d2906ea1cccc61c3ae66958e3e637db
JLA <63759042+Blu3DrAEm@users.noreply.github.com>
2021-01-20 13:00:22 -0700

    Create requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,10 @@
+# Downloads dependencies, makes scripts executable, and moves resolv.conf file
+sudo apt-get install -y openvpn
+sudo apt-get install -y expect
+sudo chmod +x openvpn.sh vpn.sh
+sudo mkdir /home/pi/resolvconfbackup
+sudo cp /etc/resolv.conf /home/pi/resolvconfbackup
+sudo rm /etc/resolv.conf
+sudo cp resolv.conf /etc
+sudo cp /home/pi/oldgregg/openvpn.sh /home/pi
+sudo cp /home/pi/oldgregg/vpn.sh /home/pi
