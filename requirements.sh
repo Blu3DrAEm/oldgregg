4f71eb8ca84eab697425b76f007425e8f68bfb30
JLA <63759042+Blu3DrAEm@users.noreply.github.com>
2021-01-20 22:42:18 -0700

    Update requirements.sh
    Updated to download the vpnbook openvpn server certificate bundle US1 by default.

--- requirements.sh
+++ requirements.sh
@@ -2,6 +2,8 @@
 sudo apt-get install -y openvpn
 sudo apt-get install -y expect
 sudo chmod +x openvpn.sh vpn.sh
+wget https://www.vpnbook.com/free-openvpn-account/VPNBook.com-OpenVPN-US1.zip
+unzip VPNBook.com-OpenVPN-US1.zip -d /home/pi/vpnbook
 sudo mkdir /home/pi/resolvconfbackup
 sudo cp /etc/resolv.conf /home/pi/resolvconfbackup
 sudo rm /etc/resolv.conf
